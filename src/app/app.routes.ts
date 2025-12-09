c3f37edfcb91aa8d4dad807ff50deffb9938e2a2
aiyazbuksh <aiyazbuksh@hotmail.com>
2025-12-10 11:38:06 +1300

    fixed routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,6 @@ export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'customer', loadComponent: () => import('./pages/customer/list.component').then(m => m.ListComponent) },
   { path: 'customer/detail/:id', loadComponent: () => import('./pages/customer/detail.component').then(m => m.DetailComponent) },
-  { path: 'customer/create-invoice', loadComponent: () => import('./pages/customer/create-invoice.component').then(m => m.CreateInvoiceComponent) },
+  { path: 'customer/create-invoice/:id?', loadComponent: () => import('./pages/customer/create-invoice.component').then(m => m.CreateInvoiceComponent) },
   //{ path: 'path/supplier/create-invoice', loadComponent: () => import('./pages/supplier/create-invoice.component').then(m => m.CreateInvoiceComponent) }
 ];
