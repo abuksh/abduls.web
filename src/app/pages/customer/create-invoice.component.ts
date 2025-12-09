9b18ec09e7ae48b5c2d22f3dae30ff927aad6fcf
aiyazbuksh <aiyazbuksh@hotmail.com>
2025-12-10 11:36:17 +1300

    fixed routing.

---
+++ src/app/pages/customer/create-invoice.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-invoice',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      create-invoice works!
+    </p>
+  `,
+  styles: ``,
+})
+export class CreateInvoiceComponent {
+
+}
---
+++ src/app/pages/supplier/create-invoice.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-create-invoice',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      Supplier Create Invoice works!
+    </p>
+  `,
+  styles: ``,
+})
+export class CreateInvoiceComponent {}
